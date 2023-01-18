d9fa41b09058004fb1aa3512b55b5d4ac12900cb
Rakharan <randhikatamar@gmail.com>
2023-01-18 20:31:43 +0700

    add files

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require('express');
+const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
+const keys = require('./config/keys')
+require('./models/User')
+require('./services/passport');
+
+mongoose.set('strictQuery', false)
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true })
+
+const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 23 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+)
+app.use(passport.initialize())
+app.use(passport.session())
+
+require('./routes/authRoutes')(app)
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT); 
